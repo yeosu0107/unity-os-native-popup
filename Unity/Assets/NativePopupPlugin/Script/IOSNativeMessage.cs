8f2a86ebec8955c343322052065f35a40114f264
sungwoo.yeo <yeosu0107@naver.com>
2021-08-15 17:22:43 +0900

    [add] 누락된 파일 추가
    - gitignore 수정

---
+++ Unity/Assets/NativePopupPlugin/Script/IOSNativeMessage.cs
@@ -0,0 +1,108 @@
+﻿#if UNITY_IOS
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace NativePopup
+{
+    public class IOSNativeMessage : MonoBehaviour, INativeMessage
+    {
+        #region IOS_FUNCTION
+
+        [DllImport("__Internal")]
+        private static extern void _init();
+
+        [DllImport("__Internal")]
+        private static extern void _ShowAlertOneButton(string title, string message, string ok);
+
+        [DllImport("__Internal")]
+        private static extern void _ShowAlertTwoButton(string title, string message, string ok, string cancel);
+
+        [DllImport("__Internal")]
+        private static extern void _ShowToast(string message, double seconds);
+
+        #endregion
+
+        private const string objectName = "NativeMessageObject";
+        private static IOSNativeMessage instance = null;
+
+        private ButtonCallback okCallback = null;
+        private ButtonCallback cancelCallback = null;
+
+        public static IOSNativeMessage GetInstance()
+        {
+            if (instance == null)
+            {
+                var emptyObj = new GameObject(objectName).AddComponent<IOSNativeMessage>();
+                instance = emptyObj.GetComponent<IOSNativeMessage>();
+
+#if !UNITY_EDITOR
+        _init();
+#endif
+            }
+
+            return instance;
+        }
+
+        public void ShowAlertOneButton(string title, string message, string okStr, ButtonCallback callback = null)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning("NativeMessage not support editor");
+            return;
+#elif !UNITY_IOS
+        return;
+#endif
+            if (callback != null)
+            {
+                okCallback += callback;
+            }
+
+            _ShowAlertOneButton(title, message, okStr);
+        }
+
+        public void ShowAlertTwoButton(string title, string message, string okStr, string cancelStr,
+            ButtonCallback okCallback = null, ButtonCallback cancelCallback = null)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning("NativeMessage not support editor");
+            return;
+#elif !UNITY_IOS
+        return;
+#endif
+            if (okCallback != null)
+            {
+                this.okCallback += okCallback;
+            }
+
+            if (cancelCallback != null)
+            {
+                this.cancelCallback += cancelCallback;
+            }
+
+            _ShowAlertTwoButton(title, message, okStr, cancelStr);
+        }
+
+        public void ShowToast(string message, double seconds)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning("NativeMessage not support editor");
+            return;
+#elif !UNITY_IOS
+        return;
+#endif
+            _ShowToast(message, seconds);
+        }
+
+        public void OnOk(string response)
+        {
+            okCallback?.Invoke(response);
+            okCallback = null;
+        }
+
+        public void OnCancel(string response)
+        {
+            cancelCallback?.Invoke(response);
+            cancelCallback = null;
+        }
+    }
+}
+#endif
---
+++ Unity/Assets/NativePopupPlugin/Script/NativeMessage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace NativePopup
+{
+    public delegate void ButtonCallback(string response);
+    public interface INativeMessage
+    {
+        void ShowAlertOneButton(string title, string message, string okStr, ButtonCallback callback = null);
+        void ShowAlertTwoButton(string title, string message, string okStr, string cancelStr, ButtonCallback okCallback = null, ButtonCallback cancelCallback = null);
+        void ShowToast(string message, double seconds);
+
+        void OnOk(string response);
+        void OnCancel(string response);
+    }
+
+    public class NativeMessage
+    {
+        public static INativeMessage GetInstance()
+        {
+            
+#if UNITY_IOS
+            return IOSNativeMessage.GetInstance();
+#elif UNITY_ANDROID
+            Debug.LogWarning("NativeMessage dose not support android platform yet.");
+            return null;
+#endif
+            Debug.LogError("NativeMessage supports only ios and android platform.");
+            return null;
+        }
+    }
+}

8a1500df34d74158562df40280e9aae0f77a8e9e
sungwoo.yeo <yeosu0107@naver.com>
2021-08-15 14:20:26 +0900

    [add] ios alert, toast 기능 추가

---
+++ iOS/IOSNativePlugin/IOSNativePlugin/IOSNativePlugin.h
@@ -0,0 +1,18 @@
+//
+//  IOSNativePlugin.h
+//  IOSNativePlugin
+//
+//  Created by sungwoo.yeo on 2021/08/01.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IOSNativePlugin.
+FOUNDATION_EXPORT double IOSNativePluginVersionNumber;
+
+//! Project version string for IOSNativePlugin.
+FOUNDATION_EXPORT const unsigned char IOSNativePluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOSNativePlugin/PublicHeader.h>
+
+

c1d31616cdd6663f13928136d06a43a732a3a1e6
sungwoo.yeo <yeosu0107@naver.com>
2021-08-15 17:11:47 +0900

    [add] unity에서 ios alert, toast open하는 기능 추가

---
+++ Unity/Assets/NativePopupPlugin/Demo/DemoScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using NativePopup;
+
+public class DemoScript : MonoBehaviour
+{
+    public void OpenAlertOne()
+    {
+        Debug.Log("open alert with one button");
+        NativeMessage.GetInstance().ShowAlertOneButton("Alert", "This is alert", "OK", response =>
+        {
+            Debug.Log($"alert callback: {response}");
+        });
+    }
+
+    public void OpenAlertTwo()
+    {
+        Debug.Log("open alert with two button");
+        NativeMessage.GetInstance().ShowAlertTwoButton("Alert", "This is alert", "OK", "Cancel",response =>
+        {
+            Debug.Log($"alert ok callback: {response}");
+        }, response =>
+        {
+            Debug.Log($"alert cancel callback: {response}");
+        });
+    }
+
+    public void OpenToast()
+    {
+        Debug.Log("open toast");
+        NativeMessage.GetInstance().ShowToast("This is toast message!", 3.5);
+    }
+}
